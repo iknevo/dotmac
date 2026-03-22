27e2a696db7e679138e558727c848555d5e06429
nevo <nevoexists@gmail.com>
2026-03-22 16:13:02 +0200

    sync

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -38,7 +38,6 @@ return {
   -- filename
   {
     "b0o/incline.nvim",
-    enabled = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       local devicons = require("nvim-web-devicons")
